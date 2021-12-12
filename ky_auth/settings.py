914498a167c3e8689198188ff30b214e4e01655e
Vysakh A V <vysakhav951@gmail.com>
2021-12-12 21:40:28 +0530

    deploy

--- ky_auth/settings.py
+++ ky_auth/settings.py
@@ -25,6 +25,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+if os.getcwd() == '/app':
+    DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
